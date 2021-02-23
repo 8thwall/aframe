76dbfa5c296ccf6c9830c3c9141958d5bf322f73
Nathan <watersnathan1@gmail.com>
2021-02-23 10:43:05 -0600

    Removing 'device-orientation-permission-ui' from default

--- src/core/scene/a-scene.js
+++ src/core/scene/a-scene.js
@@ -90,7 +90,6 @@ class AScene extends AEntity {
     this.setAttribute('inspector', '');
     this.setAttribute('keyboard-shortcuts', '');
     this.setAttribute('screenshot', '');
-    this.setAttribute('device-orientation-permission-ui', '');
     super.connectedCallback();
 
     // Renderer initialization
